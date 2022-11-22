1513e77bfa15c5ab3afb1abe92832da78144813e
srkans <serkansacma@hotmail.com>
2022-11-22 08:15:17 +0300

    hello world

---
+++ Ders1-20170729/Hoca/29_07_2017/Merhaba_CSharp/Merhaba_CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merhaba_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+        }
+
+    }
+}
