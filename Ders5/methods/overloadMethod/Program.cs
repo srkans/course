1ac996f5a0e0d0e063006a72fabb03df63f89dbf
srkans <serkansacma@hotmail.com>
2022-11-23 14:22:10 +0300

    method overloading

---
+++ Ders5/methods/overloadMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloadMethod
+{
+    class Program
+    {
+        static void Yaz()
+        {
+            Console.WriteLine("deneme");
+        }
+
+        static void Yaz(string mesaj)
+        {
+            Console.WriteLine(mesaj);
+        }
+
+        static void Main(string[] args)
+        {
+            Yaz();
+            Yaz("Biseyleryaz");
+        }
+    }
+}
