780a58c13a4cbbff0cfa3b4a280ee4c85dc24761
srkans <serkansacma@hotmail.com>
2022-11-23 16:31:54 +0300

    object oriented programming intro

--- Ders6/oop/oop/Program.cs
+++ Ders6/oop/oop/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace oop
 {
+
+    class wc2022
+    {
+        public int number;
+        string message;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            wc2022 japan = new wc2022();
+
+            japan.number = 0;
+
+            Console.WriteLine(japan.number);
+
+            wc2022 germany = new wc2022();
+
+            germany.number = 4;
+
+            Console.WriteLine(germany.number);
         }
     }
 }
