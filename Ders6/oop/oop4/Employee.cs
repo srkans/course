ea45dd1d80407d538e585c5aac714f2be45d55d4
srkans <serkansacma@hotmail.com>
2022-11-23 17:33:11 +0300

    encapsulation practice

---
+++ Ders6/oop/oop4/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop4
+{
+    class Employee
+    {
+        private string _name;
+        private string _lastName;
+        private string _address;
+        private int _id;
+        private double _salary;
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string lastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public int id
+        {
+        get { return _id; }
+        set { _id = value; }
+        }
+
+        public double salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public string showEmployeeInfo()
+        {
+            return string.Format("{0} {1} {2} {3} {4}", _name, _lastName, _address, _id, _salary);
+        }
+    }
+}
---
+++ Ders6/oop/oop4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Serkan = new Employee();
+
+            Serkan.name = "Serkan";
+            Serkan.lastName = "Sacma";
+            Serkan.address = "Istanbul";
+            Serkan.id = 963;
+            Serkan.salary = 999999999.9;
+
+            Console.WriteLine(Serkan.showEmployeeInfo());
+        }
+    }
+}
