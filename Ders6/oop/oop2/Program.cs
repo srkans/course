c54795a4771465357851a0ee718dfd6693e9959c
srkans <serkansacma@hotmail.com>
2022-11-23 16:43:47 +0300

    oop2 class with methods

---
+++ Ders6/oop/oop2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    class Room
+    {
+        private int number;
+        string message;
+
+        public void setMessage(string s)
+        {
+            message = s;
+        }
+
+        public string getMessage()
+        {
+            return message;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room serkan = new Room();
+            Room betul = new Room();
+
+            serkan.setMessage("naber");
+            Console.WriteLine(serkan.getMessage());
+        }
+    }
+}
