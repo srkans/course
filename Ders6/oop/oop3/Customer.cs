a19679d76fa0d4184e651795ccbe55fe37611cf9
srkans <serkansacma@hotmail.com>
2022-11-23 17:07:02 +0300

    constructor - destructor methods

---
+++ Ders6/oop/oop3/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string lastName { get; private set; } //private will not used 
+        public string address { get; set; }
+
+        public Customer() // constructor method
+        {
+            Console.WriteLine("Customer object has been created");
+        }
+
+        public Customer(int id) //constructor
+        {
+            this.ID = id;
+            Console.WriteLine("Customer object has been created 2");
+        }
+
+        ~Customer() //destructor method 
+        {
+            Console.WriteLine("Customer object has been terminated");
+        }
+    }
+}
---
+++ Ders6/oop/oop3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer ss = new Customer(46);
+            ss.ID = 963;
+            
+            Console.ReadLine();
+        }
+    }
+}
