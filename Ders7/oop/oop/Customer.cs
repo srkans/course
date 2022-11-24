dacf6ca126d06bbb55511337078bdaaa0f9ab53b
srkans <serkansacma@hotmail.com>
2022-11-24 09:24:55 +0300

    conditional class property

---
+++ Ders7/oop/oop/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Customer
+    {
+        public string code;
+        private int _number;
+        public int id { get; set; }
+
+        public Customer()
+        {
+            Console.WriteLine("...object created...");
+        }
+
+        public Customer(string message)
+        {
+            Console.WriteLine("...." + message + "....");
+        }
+
+        public int number
+        {
+            get { return _number; }
+            set { if(value<0)
+                    {
+                    _number = 0;
+                    }
+                   else
+                {
+                    _number = value;
+                }
+                }
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine(bringInfo());
+        }
+
+        private string bringInfo()
+        {
+            return "number : " + _number;
+        }
+    }
+}
---
+++ Ders7/oop/oop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer("welcome c");
+
+            c.number = -99999;
+            c.showInfo();
+        }
+    }
+}
