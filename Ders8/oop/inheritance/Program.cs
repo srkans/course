c2da75c3cbd8a96d56f539a772e1040adcdd1fc3
srkans <serkansacma@hotmail.com>
2022-11-24 12:05:58 +0300

    inheritance introduction

---
+++ Ders8/oop/inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class Emplyoee
+    {
+        public int empID { get; set; }
+        public string name { get; set; }
+
+        protected void Trial()
+        {
+            ;
+        }
+    }
+
+    class Manager:Emplyoee
+    {
+        public string carID { get; set; }
+
+        public void Greet()
+        {
+            ;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emplyoee ece = new Emplyoee();
+            Manager ali = new Manager();
+
+            //inheritance 
+        }
+    }
+}
