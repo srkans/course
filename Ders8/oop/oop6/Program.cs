1cef3175e891b8c902be1a9aaa49ee03da57e78a
srkans <serkansacma@hotmail.com>
2022-11-24 14:15:46 +0300

    override instant methods

---
+++ Ders8/oop/oop6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop6
+{
+    class Personel
+    {
+        public int perID { get; set; }
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return perID + " " + name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel p = new Personel();
+            p.perID = 46;
+            p.name = "Abuzittin";
+
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
