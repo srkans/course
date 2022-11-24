e965a2828d968561b93d243c2af5504755f50ab7
srkans <serkansacma@hotmail.com>
2022-11-24 10:10:39 +0300

    oop practice

---
+++ Ders8/oop/oop/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Book
+    {
+        private int _id;
+        private string _bookName;
+        private string _authorName;
+        private double _price;
+
+        public Book()
+        {
+
+        }
+
+        public Book(int number, string str)
+        {
+            id = number;
+            bookName = str;
+        }
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string bookName
+        {
+            get { return _bookName; }
+            set { _bookName = value; }
+        }
+
+        public string authorName
+        {
+            get { return _authorName; }
+            set { _authorName = value; }
+        }
+
+        public double price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string showInfo()
+        {
+            return string.Format("{0} {1} {2} {3}", _id, _bookName, _authorName, _price);
+        }
+    }
+}
---
+++ Ders8/oop/oop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+
+            Book book2 = new Book(999, Console.ReadLine());
+            Console.WriteLine(book2.showInfo());
+            book2.id = 963;
+            book2.bookName = "Olasılıksız";
+            book2.authorName = "Adam Fever";
+            book2.price = 36;
+
+            Console.WriteLine(book2.showInfo());
+
+
+        }
+    }
+}
