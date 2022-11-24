e3560f033b3c7fc730d9b0d33f12f26cc0bedf82
srkans <serkansacma@hotmail.com>
2022-11-24 12:55:39 +0300

    inheritance practice

---
+++ Ders8/oop/oop3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3
+{
+    class Circle
+    {
+        public int r { get; set; }
+
+        public double calcArea()
+        {
+            return Math.PI * r * r;
+        }
+    }
+
+    class Cylinder:Circle
+    {
+        public int h { get; set; }
+
+        public double cylVolume()
+        {
+            return calcArea() * h;
+        }
+    }
+
+    class Cone:Cylinder
+    {
+        public double coneVolume()
+        {
+            return cylVolume()*0.333;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.r = 4;
+            Console.WriteLine(c.calcArea());
+
+            Cylinder cyl = new Cylinder();
+            cyl.r = 3;
+            cyl.h = 4;
+            Console.WriteLine(cyl.cylVolume());
+
+            Cone con = new Cone();
+            con.r = 3;
+            con.h = 4;
+            Console.WriteLine(con.coneVolume());
+          
+        }
+    }
+}
