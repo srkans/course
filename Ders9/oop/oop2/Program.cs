434db4aa66f693096aa0a73efa3ca6f4baf871d8
srkans <serkansacma@hotmail.com>
2022-11-24 16:26:39 +0300

    ArrayList collections

---
+++ Ders9/oop/oop2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    class Personel
+    {
+        public int PerID { get; set; }
+        public string Ad { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList personeller = new ArrayList();
+
+            Personel p1 = new Personel();
+            Personel p2 = new Personel();
+            p1.PerID = 123;
+            p1.Ad = "Cevdet";
+            p2.PerID = 222;
+            p2.Ad = "Selami";
+            personeller.Add(p1);
+            personeller.Add(p2);
+
+            foreach (Personel p in personeller)
+                Console.WriteLine("ID:{0} Ad:{1}", p.PerID, p.Ad);
+        }
+    }
+}
