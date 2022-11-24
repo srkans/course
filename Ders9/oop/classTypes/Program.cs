a66ae3e7926715319687a7abceb3451bcbec3bb3
srkans <serkansacma@hotmail.com>
2022-11-24 16:08:21 +0300

    abstract sealed static class

---
+++ Ders9/oop/classTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "deneme";
+
+            str.ToUpper();
+
+            Random rnd = new Random();
+
+            Generated g = new Generated();
+           
+
+            staClass.Deneme(); // * 
+        }
+    }
+}
---
+++ Ders9/oop/classTypes/classes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTypes
+{
+
+    abstract class absClass 
+    {
+        public int number { get; set; }
+    }
+
+    class Generated : absClass
+    {
+        public string name { get; set}
+    }
+
+    sealed class sealClass : Generated
+    {
+
+    }
+
+    static class staClass
+    {
+        public static void Deneme()
+        {
+
+        }
+
+           
+    }
+   
+
+}
