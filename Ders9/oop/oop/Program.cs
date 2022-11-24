8745a038ff8ec7547c2d979c4fb297348be33782
srkans <serkansacma@hotmail.com>
2022-11-24 16:11:16 +0300

    constructor destructor sample

--- Ders9/oop/oop/Program.cs
+++ Ders9/oop/oop/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace oop
 {
+    class Buton
+    {
+        static int sayac = 0;
+
+        public Buton()
+        {
+            //sayac++;
+            Console.WriteLine(++sayac);
+        }
+
+        ~Buton()
+        {
+            Console.WriteLine(--sayac);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+            Buton b1 = new Buton();
+            Buton b2 = new Buton();
+            Buton b3 = new Buton();
+            Console.ReadLine();
         }
     }
 }
