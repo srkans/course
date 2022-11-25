336927b519dcd6dcf54358ea4054c3e62bf6c5c6
srkans <serkansacma@hotmail.com>
2022-11-25 14:21:56 +0300

    interface practice

---
+++ Ders11/Interface_1/Interface-3/Classes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_3
+{
+    interface IBoyanabilir
+    {
+        string boyaRengi { get; set; }
+    }
+
+    class Boyahane
+    {
+        public void Boya(IBoyanabilir arac,ConsoleColor yeniRenk)
+        {
+            arac.boyaRengi = yeniRenk.ToString();
+            Console.ForegroundColor = yeniRenk;
+        }
+
+    }
+
+
+    class Tasit
+        {
+            public string ad { get; set; }
+            public int hiz { get; set; }
+            public byte tekerlekSayisi { get; set; }
+            public string renk { get; set; }
+        }
+
+        class Ferrari : Tasit,IBoyanabilir
+        {
+            public string boyaRengi
+            {
+                get
+                {
+                    return this.renk;
+                }
+                set
+                {
+                    this.renk = value;
+                }
+            }
+        }
+
+    class Serce:Tasit,IBoyanabilir
+    {
+        public string boyaRengi
+        {
+            get
+            {
+                return this.renk;
+            }
+            set
+            {
+                this.renk = value;
+            }
+        }
+    }
+
+    class BMX : Tasit
+    {
+        
+    }
+}
---
+++ Ders11/Interface_1/Interface-3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boyahane b = new Boyahane();
+
+            Ferrari f = new Ferrari();
+            Serce s = new Serce();
+            BMX bmx = new BMX();
+
+            b.Boya(f, ConsoleColor.Blue);
+            Console.WriteLine(f.boyaRengi);
+
+            b.Boya(s, ConsoleColor.DarkGreen);
+            Console.WriteLine(s.boyaRengi);
+
+            //bmx IBoyanabilir arayuzunu uygulamadığı için çalışmayacaktır...
+            //b.Boya(bmx, ConsoleColor.Red);
+
+            IBoyanabilir b1 = new Ferrari();
+            IBoyanabilir b2 = new Serce();
+            //IBoyanabilir b3 = new Bmx();
+
+            //((Ferrari)b1).Ad;
+        }
+    }
+}
