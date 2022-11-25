2df123a8cc73ca193e02fdca6af648138600026c
srkans <serkansacma@hotmail.com>
2022-11-25 12:03:04 +0300

    oop practice

---
+++ Ders10/practice/oop1/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    class Car
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("Drive car");
+        }
+    }
+
+    class Ferrari : Car
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Drive Ferrari");
+        }
+    }
+
+    class Murat124 : Car
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Drive Haci Murat");
+        }
+    }
+}
---
+++ Ders10/practice/oop1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car regular = new Car();
+            Car ferrari = new Ferrari();
+            Car tofas = new Murat124();
+
+            regular.Drive();
+
+            ferrari.Drive();
+
+            tofas.Drive();
+        }
+    }
+}
