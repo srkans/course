a5ab48674207ea8beaa6d586500b9c01c48d5e6a
srkans <serkansacma@hotmail.com>
2022-11-25 11:34:46 +0300

    sortedList

---
+++ Ders10/practice/sortedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList list = new SortedList();
+
+            list.Add("serkan", "dfokgjfdop fgojdhgoıp *0ıewfpodfşl");
+            list.Add("betul", "asdşlkdfşlo reyurtopı dşflkvmdflk sdşglfk");
+            list.Add("susu", "sdfşsdğ sdfsdf 3w4e sdfsdf weer3");
+
+            foreach(object obj in list.Keys)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine(list["betul"]);
+        }
+    }
+}
