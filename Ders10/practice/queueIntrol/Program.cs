3dd3beb8397de8a89c493a90c40205292531fa46
srkans <serkansacma@hotmail.com>
2022-11-25 11:20:08 +0300

    collections - queue

---
+++ Ders10/practice/queueIntrol/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queueIntrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue("betul");
+            q.Enqueue("serkan");
+            q.Enqueue("susu");
+
+            Console.WriteLine(q.Peek());
+
+            Console.WriteLine(q.Dequeue());
+
+            Console.WriteLine(q.Peek());
+
+            
+
+        }
+    }
+}
