f5a293b3236eccc428d793991d6150bdbd965702
srkans <serkansacma@hotmail.com>
2022-11-25 12:21:16 +0300

    throw--exception handling basic sample

---
+++ Ders10/practice/throwBasic/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throwBasic
+{
+    class Customer
+    {
+        private int id;
+
+        public Customer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        ~Customer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int cusID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                ;
+            }
+        }
+
+        public string showInfo(string a)
+        {
+            return " ";
+        }
+    }
+}
---
+++ Ders10/practice/throwBasic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throwBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer();
+            c.cusID = 1;
+            c.showInfo("b");
+        }
+    }
+}
