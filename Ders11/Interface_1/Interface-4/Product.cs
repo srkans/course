ea6963ec6d376a1aeb8a9237944a45657cae4d71
srkans <serkansacma@hotmail.com>
2022-11-25 14:46:16 +0300

    IComparable interface

---
+++ Ders11/Interface_1/Interface-4/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_4
+{
+    class Product : IComparable
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            int result;
+
+            if (id > ((Product)obj).id)
+                result = 1;
+            else if (id < ((Product)obj).id)
+                result = -1;
+            else
+                result = 0;
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return id + " " + name + " " + price;
+        }
+    }
+}
---
+++ Ders11/Interface_1/Interface-4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list1 = new ArrayList { 111, 77, 99, 172, 56 };
+            list1.Sort();
+
+            foreach(int i in list1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("------------------------");
+
+            ArrayList products = new ArrayList();
+            Product p1 = new Product();
+            p1.id = 2;
+            p1.name = "Pen";
+            p1.price = 9;
+
+            Product p2 = new Product();
+            p2.id = 1;
+            p2.name = "Book";
+            p2.price = 13;
+
+            products.Add(p1);
+            products.Add(p2);
+
+            products.Sort();  //object must implement Icomparable !!!
+
+            foreach(Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
