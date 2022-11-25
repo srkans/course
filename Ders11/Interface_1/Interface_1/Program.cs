42bcea39a558394e4cac151e147509a0b288f380
srkans <serkansacma@hotmail.com>
2022-11-25 13:50:38 +0300

    interface introduction

---
+++ Ders11/Interface_1/Interface_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_1
+{
+    class A : IInterface
+    {
+        public void Deneme ()
+        {
+            Console.WriteLine("Class A");
+        }
+    }
+
+    class B : IInterface
+    {
+        public void Deneme()
+        {
+            Console.WriteLine("Class B");
+        }
+    }
+
+    interface IInterface
+    {
+        void Deneme();
+    }
+    //1-Arayuz icerisinde degisken tanımlaması yapılamaz
+    //2-Arayuz icerisinde metodun tamamı olmaz
+    //3-Arayuz icerisinde her sey public'tir erisim belirleyici yazılmaz.
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Deneme();
+
+            B b = new B();
+            b.Deneme();
+        }
+    }
+}
---
+++ Ders11/Interface_1/enum_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_1
+{
+    public enum KDV { Gida = 1, Icecek, Giyim = 8, Diger = 18, Luks = 25 }
+
+    public enum Days :byte { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Serkannnn");
+
+            for(int i = 0; i<16; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)i;
+                Console.WriteLine("Serkannnn");
+            }
+
+            Console.WriteLine((int)KDV.Luks);
+
+            Console.WriteLine((int)Days.Thursday);
+        }
+    }
+}
