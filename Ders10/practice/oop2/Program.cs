89f32ff6c770cffc267e30c203ada3df8921fb4d
srkans <serkansacma@hotmail.com>
2022-11-25 12:09:13 +0300

    oop practice

---
+++ Ders10/practice/oop2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    class Customer
+    {
+        public string customerName { get; set; }
+
+        public override string ToString()
+        {
+            return "Customer name : " + customerName;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer();
+
+            c.customerName = "Betul Altu";
+
+            Console.WriteLine(c);
+        }
+    }
+}
