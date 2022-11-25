6f45b5c0a7e6b145188e54ff55d20bebf263a1b0
srkans <serkansacma@hotmail.com>
2022-11-25 11:09:07 +0300

    stack : LIFO

---
+++ Ders10/practice/stackIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack:LIFO
+
+            Stack s = new Stack();
+
+            s.Push("ferrari");
+            s.Push("porsche");
+            s.Push("mercedes");
+
+            Console.WriteLine("Top is : " + s.Peek());
+
+            Console.WriteLine("Remove from stack : " + s.Pop());
+
+            Console.WriteLine(s.Peek());
+            
+        }
+    }
+}
