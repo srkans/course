366915bc309f42be4675af5a911d44794e24fa57
srkans <serkansacma@hotmail.com>
2022-11-25 10:30:09 +0300

    arrayList - class practice

---
+++ Ders10/practice/practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store s = new Store();
+
+            Product p1 = new Product();
+            p1.id = 1;
+            p1.name = "Pen";
+
+            for (int i = 0; i<13; i++)
+            {
+                Console.WriteLine(s.addProduct(p1));
+            }
+
+            s.removeProduct(p1);
+
+            foreach(Product p in s.List())
+            {
+                Console.WriteLine(p.id + " " + p.name);
+            }
+        }
+    }
+}
---
+++ Ders10/practice/practice/Store.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class Store
+    {
+        ArrayList store = new ArrayList(10);
+
+        public string addProduct(Product p)
+        {
+            string str = "Store is full";
+            if (store.Count<10)
+            {
+                store.Add(p);
+                str = "Product added";
+            }
+            return str;
+        }
+
+        public void removeProduct(Product p)
+        {
+            store.Remove(p);
+        }
+
+        public ArrayList List()
+        {
+            return store;
+        }
+    }
+}
