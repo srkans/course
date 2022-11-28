2a1f769e7ae4fac14d74f00e8a2794bb8f27c21e
srkans <serkansacma@hotmail.com>
2022-11-28 15:33:57 +0300

    events introduction

---
+++ Ders13/Events/Events/Button.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void delButton();
+    class Button
+    {
+        public event delButton Click;
+
+        public void Run()
+        {
+            Click();
+        }
+    }
+}
---
+++ Ders13/Events/Events/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Button b = new Button();
+
+            //b.Click += new delButton(D_Click);
+
+            b.Click += D_Click;
+
+            b.Run();
+
+        }
+
+        private static void D_Click()
+        {
+            Console.WriteLine("Button object has been clicked");
+        }
+    }
+}
