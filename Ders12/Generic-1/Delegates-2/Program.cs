dbc1bf587c476211510ca26c52d69414223275d4
srkans <serkansacma@hotmail.com>
2022-11-28 13:32:10 +0300

    anonymous method

---
+++ Ders12/Generic-1/Delegates-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_2
+{
+    public delegate void delSerkan(string s); 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delSerkan d1 = delegate (string str)
+            {
+                Console.WriteLine("anonymous method : " + str);
+            };
+
+            d1.Invoke("HeyHeyHey");
+        }
+    }
+}
