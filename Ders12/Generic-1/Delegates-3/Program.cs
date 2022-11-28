2ab19820e720eea5e5dcdbee650eb464b6b94fe0
srkans <serkansacma@hotmail.com>
2022-11-28 13:52:56 +0300

    anonymous method practice

---
+++ Ders12/Generic-1/Delegates-3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_3
+{
+    class Program
+    {
+        static bool oddNumber(int s)
+        {
+            return s % 2 == 1 ? true : false;
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.Add(12);
+            numbers.Add(31);
+            numbers.Add(9);
+            numbers.Add(6);
+            numbers.Add(3);
+            numbers.Add(369);
+
+            foreach(int i in numbers.FindAll(oddNumber))
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach(int i in numbers.FindAll(delegate (int i) { return i % 2 == 1 ? true : false; } ))
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach(int i in numbers.FindAll(i=> i % 2 == 1 ? true:false))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
