aca232711aea70b0683bb54bdd7e1e1c609c89e9
srkans <serkansacma@hotmail.com>
2022-11-28 09:57:26 +0300

    generic class

---
+++ Ders12/Generic-1/Generic-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_2
+{
+    class Betul<T>
+    {
+        T[] array;
+
+        public Betul()
+        {
+            array = new T[10];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Betul<int> b = new Betul<int>();
+
+            Betul<string> s = new Betul<string>();
+        }
+    }
+}
