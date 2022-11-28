002e0ab8cd8bb99b865b7f7466e23e78c04a479b
srkans <serkansacma@hotmail.com>
2022-11-28 16:37:01 +0300

    structs

---
+++ Ders13/Events/struct-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_1
+{
+    struct point2D //if the data is less then 16byte then use struct
+    {
+        public int X;
+        public int Y;
+
+        public point2D(int x, int y) 
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point2D point;
+
+            point.X = 20;
+            point.Y = 12;
+
+            int number = new int(); //sample
+            Console.WriteLine(number);
+
+
+        }
+    }
+}
