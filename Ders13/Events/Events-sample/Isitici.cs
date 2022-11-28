65729274c97c31484c61f58f6c5b3452f26428dd
srkans <serkansacma@hotmail.com>
2022-11-28 15:58:54 +0300

    events boiler sample

---
+++ Ders13/Events/Events-sample/Isitici.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_sample
+{
+    public delegate void olayYakalayici();
+    class Isitici
+    {
+        public int sicaklik { get; set; }
+
+        public event olayYakalayici Kabarciklan;
+        public event olayYakalayici sesCikar;
+        public event olayYakalayici fokurda;
+
+        public void Isıt()
+        {
+            sicaklik++;
+
+            if(Kabarciklan!=null && sicaklik>=20)
+            {
+                Kabarciklan();
+            }
+            if(sesCikar!=null && sicaklik >= 40)
+            {
+                sesCikar();
+            }
+            if(fokurda!=null && sicaklik>=100)
+            {
+                fokurda();
+            }
+        }
+    }
+}
---
+++ Ders13/Events/Events-sample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Isitici a = new Isitici();
+
+            a.Kabarciklan += A_Kabarciklan;
+            a.sesCikar += A_SesCikar;
+            a.fokurda += A_Fokurda;
+
+            for(int i = 0; i<110; i++)
+            {
+                Thread.Sleep(250);
+
+                a.Isıt();
+            }
+        }
+
+        private static void A_Fokurda()
+        {
+            Console.WriteLine("Fokurda...");
+        }
+
+        private static void A_SesCikar()
+        {
+            Console.WriteLine("ses cikar...");
+        }
+
+        private static void A_Kabarciklan()
+        {
+            Console.WriteLine("kabarcik...");
+        }
+    }
+}
