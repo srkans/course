3f325825c8f8a7587b793c694643c98a1c47ef42
srkans <serkansacma@hotmail.com>
2022-11-28 16:30:02 +0300

    throw exception -- generate userdefined exception

---
+++ Ders13/Events/exceptionHandling-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandling_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3; 
+
+            if(number ==3)
+            {
+                throw new cantbeThree();
+            }
+        }
+    }
+}
---
+++ Ders13/Events/exceptionHandling-2/cantbeThree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandling_2
+{
+    class cantbeThree:Exception //we have generated our exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Value can't be three...";
+            }
+        }
+    }
+}
