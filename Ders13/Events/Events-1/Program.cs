e7ff7423aca9335fd9e251d1b145d896beffc35e
srkans <serkansacma@hotmail.com>
2022-11-28 15:45:32 +0300

    Events practice

---
+++ Ders13/Events/Events-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage s = new Storage();
+            s.isFull += D_isFull;
+
+            Product p = new Product();
+
+            p.id = 123;
+            p.name = "Paper";
+
+            for(int i = 0; i<12; i++)
+            {
+                s.addProduct(p);
+            }
+        }
+
+        private static void D_isFull()
+        {
+            Console.WriteLine("storage is full...");
+        }
+    }
+}
---
+++ Ders13/Events/Events-1/Storage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_1
+{
+    public delegate void delStorage();
+    class Storage
+    {
+        public event delStorage isFull;
+
+        List<Product> storage = new List<Product>();
+
+        public void addProduct(Product p)
+        {
+            if(storage.Count<10)
+            {
+                storage.Add(p);
+            }
+
+            if(isFull != null && storage.Count == 10)
+            {
+                isFull();
+            }
+        }
+    }
+}
