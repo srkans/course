5181cb63e9d488242afd40fd1c1fdc6f2ec70792
srkans <serkansacma@hotmail.com>
2022-11-29 12:20:24 +0300

    IO-streamwriter

---
+++ Ders14/IO/IO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter tw = new StreamWriter(@"C:\Users\Serkan\Desktop\course\Ders14\deneme2.txt", true);
+            StreamWriter sw = new StreamWriter(@"C:\Users\Serkan\Desktop\course\Ders14\deneme.txt",true);
+
+
+
+            for (int i = 0; i < 11; i++)
+            {
+                sw.WriteLine("Sayi : {0}", i);
+                tw.WriteLine("stream writer introduction");
+            }
+
+            sw.Close();
+            tw.Close();
+        }
+    }
+}
