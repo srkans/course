cac86a34b4a0fb0c6b898d71571c839097199c9c
srkans <serkansacma@hotmail.com>
2022-11-29 13:43:34 +0300

    IComparable practice

---
+++ Ders14/IO/IComparable-1/Personel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparable_1
+{
+    class Personel : IComparable
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double salary { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            int result = 0;
+            Personel p = (Personel)obj;
+
+            if(Employees.order == Order.id)
+            {
+                result = this.id > p.id ? 1 : -1;
+            }
+            else if(Employees.order == Order.name)
+            {
+                result = this.name.CompareTo(p.name);
+            }
+            else if (Employees.order == Order.salary)
+            {
+                result = this.salary > p.salary ? 1 : -1;
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", id, name, salary);
+        }
+    }
+
+    public enum Order { id, name, salary}
+
+    class Employees
+    {
+        List<Personel> employees = new List<Personel>();
+
+        public static Order order;
+
+        public void addEmployee(Personel prs)
+        {
+            employees.Add(prs);
+        }
+
+        public void List()
+        {
+            foreach(Personel p in employees)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("----------");
+        }
+
+        public List<Personel> toOrder(Order xorder)
+        {
+            order = xorder;
+            employees.Sort();
+            return employees;
+        }
+    }
+}
---
+++ Ders14/IO/IComparable-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparable_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees employees = new Employees();
+
+            employees.addEmployee(new Personel() { id = 12, name = "Cevdet", salary = 2000 });
+
+            employees.addEmployee(new Personel() { id = 9, name = "Hasan", salary = 2100 });
+
+            employees.addEmployee(new Personel() { id = 15, name = "Salih", salary = 1900 });
+
+            employees.addEmployee(new Personel() { id = 6, name = "Mustafa", salary = 1700 });
+
+            employees.List();
+
+            foreach(Personel p in employees.toOrder(Order.name))
+            {
+                Console.WriteLine(p);
+            }
+
+            foreach (Personel p in employees.toOrder(Order.id))
+            {
+                Console.WriteLine(p);
+            }
+
+            foreach (Personel p in employees.toOrder(Order.salary))
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
