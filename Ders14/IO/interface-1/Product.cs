33ac9b86e6ef86279ba3abd36fee2970045727c3
srkans <serkansacma@hotmail.com>
2022-11-29 13:17:53 +0300

    interface practice

---
+++ Ders14/IO/interface-1/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_1
+{
+    abstract class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+
+    interface IPerishable
+    {
+        DateTime skt { get; set; }
+    }
+
+    class Milk: Product, IPerishable
+    {
+        private DateTime _skt;
+
+        public DateTime skt
+        {
+            get
+            {
+                return _skt;
+            }
+            set
+            {
+                _skt = value;
+            }
+        }
+    }
+
+    class Ball : Product
+    {
+
+    }
+}
---
+++ Ders14/IO/interface-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball b = new Ball();
+            Milk m = new Milk();
+
+           
+        }
+    }
+}
