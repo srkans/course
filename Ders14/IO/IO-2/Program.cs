5f7226f4c06a219ee6c44702a75365bb93014ec5
srkans <serkansacma@hotmail.com>
2022-11-29 12:31:44 +0300

    IO-StreamReader

---
+++ Ders14/IO/IO-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Serkan\Desktop\course\Ders14\data.txt");
+
+            //  Console.WriteLine(sr.ReadToEnd());
+
+            string row;
+            int count = 1;
+
+            do
+            {
+                row = sr.ReadLine();
+                if (count %2 == 1)
+                {
+                    if(row != null)
+                    {
+                        Console.WriteLine(count + "-" + row);
+                    }
+                }
+                count++;
+            } while (row != null);
+
+            sr.Close();
+        }
+    }
+}
