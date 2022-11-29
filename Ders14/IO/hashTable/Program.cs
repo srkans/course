d0a1e2df7fb7d1bfca885853a92690efa661db59
srkans <serkansacma@hotmail.com>
2022-11-29 13:54:39 +0300

    hashTable practice

---
+++ Ders14/IO/hashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht[1] = new object();
+            ht[2] = new object();
+
+            ht.Add(3, new object());
+
+           Console.WriteLine(ht[3]);
+        }
+    }
+}
