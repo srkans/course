235cf9c8e9802db617fa60e3883c0c68870e39c8
srkans <serkansacma@hotmail.com>
2022-11-29 15:52:51 +0300

    SQL introduction

---
+++ Ders15/SQLQuery1.sql
@@ -0,0 +1,11 @@
+USE Calisma;
+
+Select * From Personeller;
+
+Select perAd , perSoyad From Personeller;
+
+Select * From Personeller Where ID = 3;
+
+Select * From Personeller Order By Maas Desc;
+
+Select * From Personeller Order By Maas Asc;
