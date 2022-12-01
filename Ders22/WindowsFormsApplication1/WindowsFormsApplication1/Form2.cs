92d06fbef0b0903fe706d9ca2cff0633f07d6fbc
srkans <serkansacma@hotmail.com>
2022-12-01 20:25:19 +0300

    keydown, lefttoprightdown

---
+++ Ders22/WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Right)
+            {
+                label1.Left += 5;
+            }
+            if (e.KeyData == Keys.Left)
+            {
+                label1.Left -= 5;
+            }
+            if (e.KeyData == Keys.Up)
+            {
+                label1.Top -= 5;
+            }
+            if (e.KeyData == Keys.Down)
+            {
+                label1.Top += 5;
+            }
+
+        }
+    }
+}
