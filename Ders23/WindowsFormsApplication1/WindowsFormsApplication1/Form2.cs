65723a60e13e1d59158b3e7a7f314328603564ba
srkans <serkansacma@hotmail.com>
2022-12-02 13:12:59 +0300

    picture box - file info - directory

---
+++ Ders23/WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+       
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+
+            string strPath = folderBrowserDialog1.SelectedPath;
+
+            foreach (string file in Directory.GetFiles(strPath))
+            {
+                FileInfo fi = new FileInfo(file);
+                if(fi.Extension == ".jpg")
+                {
+                    listBox1.Items.Add(fi.Name);
+                }
+            } 
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = folderBrowserDialog1.SelectedPath + @"\" + listBox1.SelectedItem;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
