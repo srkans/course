398d89198301ad7da4e4dc60fd7d407829b6c14c
srkans <serkansacma@hotmail.com>
2022-12-02 20:41:55 +0300

    tabPage

--- Ders24/WindowsFormsApplication1/WindowsFormsApplication1/Form8.cs
+++ Ders24/WindowsFormsApplication1/WindowsFormsApplication1/Form8.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
---
+++ Ders24/WindowsFormsApplication1/WindowsFormsApplication1/Form9.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form9 : Form
+    {
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TabPage page = new TabPage();
+            Button btn = new Button();
+            btn.Text = "Deneme";
+            btn.Dock = DockStyle.Fill;
+            page.Controls.Add(btn);
+            page.Text = "DENEME";
+            tabControl1.TabPages.Add(page);
+        }
+    }
+}
