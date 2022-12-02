5934c0d503d84a357d4c3996dd766ab380ab1b83
srkans <serkansacma@hotmail.com>
2022-12-02 17:53:07 +0300

    multi form practice

---
+++ Ders24/WindowsFormsApplication1/WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        public void getDate(string str)
+        {
+            textBox1.Text = str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form4 frm = new Form4();
+            //frm.Text = textBox1.Text;
+            //1-Access Modifier
+            //frm.textBox1.Text = textBox1.Text;
+            //2-Property
+            //frm.Metin = textBox1.Text;
+            //3-ctor
+            //Form4 frm = new Form4(textBox1.Text);
+
+            //Ters Yonde Veri Gonderme
+            Form4 frm = new Form4(textBox1.Text, this);
+            frm.Text = textBox1.Text;
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ Ders24/WindowsFormsApplication1/WindowsFormsApplication1/Form4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        public Form4(string str)
+        {
+            InitializeComponent();
+            textBox1.Text = str;
+        }
+
+        Form3 _frm;
+        public Form4(string str, Form3 frm)
+        {
+            InitializeComponent();
+            textBox1.Text = str;
+            _frm = frm;
+        }
+
+        public string Metin
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            //MessageBox.Show(e.Start.ToLongDateString());
+            _frm.getDate(e.Start.ToLongDateString());
+            this.Close();
+        }
+    }
+}
