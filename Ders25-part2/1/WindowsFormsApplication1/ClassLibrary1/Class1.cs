009d3690dbdc66f900cb0dc2ceb10f5c48ddfd02
srkans <serkansacma@hotmail.com>
2022-12-05 13:38:01 +0300

    ClassLibrary

---
+++ Ders25-part2/1/WindowsFormsApplication1/ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Personel
+    {
+
+    }
+    public class Musteri
+    {
+
+    }
+
+    public class Urun
+    {
+
+    }
+}
