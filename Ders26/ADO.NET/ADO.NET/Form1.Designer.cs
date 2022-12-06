36c9b72bf184ea58ac70fd861832dfdb18ea1152
srkans <serkansacma@hotmail.com>
2022-12-06 12:34:27 +0300

    ADO.NET

---
+++ Ders26/ADO.NET/ADO.NET/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ADO.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtAddNew = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnQuantity = new System.Windows.Forms.Button();
+            this.txtBolumID = new System.Windows.Forms.TextBox();
+            this.txtBolumAdi = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(37, 24);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(72, 43);
+            this.btnList.TabIndex = 0;
+            this.btnList.Text = "Show Records";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(232, 47);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtAddNew
+            // 
+            this.txtAddNew.Location = new System.Drawing.Point(208, 22);
+            this.txtAddNew.Name = "txtAddNew";
+            this.txtAddNew.Size = new System.Drawing.Size(100, 20);
+            this.txtAddNew.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(37, 94);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnQuantity
+            // 
+            this.btnQuantity.Location = new System.Drawing.Point(37, 236);
+            this.btnQuantity.Name = "btnQuantity";
+            this.btnQuantity.Size = new System.Drawing.Size(75, 23);
+            this.btnQuantity.TabIndex = 4;
+            this.btnQuantity.Text = "Records";
+            this.btnQuantity.UseVisualStyleBackColor = true;
+            this.btnQuantity.Click += new System.EventHandler(this.btnQuantity_Click);
+            // 
+            // txtBolumID
+            // 
+            this.txtBolumID.Location = new System.Drawing.Point(343, 94);
+            this.txtBolumID.Name = "txtBolumID";
+            this.txtBolumID.Size = new System.Drawing.Size(100, 20);
+            this.txtBolumID.TabIndex = 5;
+            // 
+            // txtBolumAdi
+            // 
+            this.txtBolumAdi.Location = new System.Drawing.Point(343, 132);
+            this.txtBolumAdi.Name = "txtBolumAdi";
+            this.txtBolumAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtBolumAdi.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(449, 94);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(28, 20);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "?";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(323, 159);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(418, 159);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(312, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 128);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Update Records";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 348);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtBolumAdi);
+            this.Controls.Add(this.txtBolumID);
+            this.Controls.Add(this.btnQuantity);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtAddNew);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtAddNew;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnQuantity;
+        private System.Windows.Forms.TextBox txtBolumID;
+        private System.Windows.Forms.TextBox txtBolumAdi;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Ders26/ADO.NET/ADO.NET/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO.NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = new SqlConnection(strConn);
+            cmd.CommandText = "SELECT * FROM Bolum";
+
+            cmd.Connection.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            listBox1.Items.Add(dr.GetName(0) + " " + dr.GetName(1));
+
+            while(dr.Read())
+            {
+                //MessageBox.Show(dr["BolumID"] + " " + dr[1].ToString());
+                listBox1.Items.Add(dr["BolumID"] + " " + dr[1].ToString());
+            }
+            cmd.Connection.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+            SqlConnection connection = new SqlConnection(strConn);
+
+            connection.Open();
+
+            // SqlCommand cmd = new SqlCommand("INSERT INTO Bolum VALUES('Yeni Bolum')",connection);
+
+            //SqlCommand cmd = new SqlCommand("INSERT INTO Bolum VALUES('"+ textBox1.Text  +"')", connenction); // injection risk
+
+            //use commands below
+
+           // SqlCommand cmd = new SqlCommand("INSERT INTO Bolum VALUES(@ad)", connection);
+
+           //nonquery
+           SqlCommand cmd = new SqlCommand("if not exists(SELECT * FROM Bolum WHERE BolumAdi =@ad)INSERT INTO Bolum VALUES(@ad)", connection);
+
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@ad", txtAddNew.Text);
+
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void btnQuantity_Click(object sender, EventArgs e)
+        {
+            string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(BolumID) FROM Bolum", conn);
+
+            int count = (int)cmd.ExecuteScalar(); //if only one value returns use executeScalar
+
+            this.Text = "Kayit Sayisi : " + count;
+
+            conn.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Bolum WHERE BolumID=@id", conn);
+
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@id", txtBolumID.Text);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            if(dr.HasRows)
+            {
+                txtBolumAdi.Text = dr["BolumAdi"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Kayıt Bulunamadı...", "Uyarı", MessageBoxButtons.OK);
+            }
+
+            conn.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("UPDATE Bolum SET BolumAdi = @ad where BolumID = @id", conn);
+
+            cmd.Parameters.Clear();
+
+            cmd.Parameters.AddWithValue("@id", txtBolumID.Text);
+            cmd.Parameters.AddWithValue("@ad", txtBolumAdi.Text);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Emin misiniz?", "UYARI", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                int ID = int.Parse(listBox1.Text.Substring(0, listBox1.Text.IndexOf(' ')));
+                txtBolumID.Text = ID.ToString();
+
+                string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+                SqlConnection conn = new SqlConnection(strConn);
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("DELETE Bolum WHERE BolumID = @id", conn);
+
+                cmd.Parameters.Clear();
+
+                cmd.Parameters.AddWithValue("@id", txtBolumID.Text);
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+            }
+        }
+    }
+}
