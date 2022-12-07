8858766574caa93b889b89beba9154b4b668811c
srkans <serkansacma@hotmail.com>
2022-12-07 13:38:23 +0300

    treeview - multiplequery-sql

---
+++ Ders29/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.btnMARS = new System.Windows.Forms.Button();
+            this.btnReadfromXML = new System.Windows.Forms.Button();
+            this.btnSavetoXML = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(23, 59);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(167, 252);
+            this.treeView1.TabIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(319, 96);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(319, 138);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(319, 179);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(319, 222);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(319, 261);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(100, 20);
+            this.txtBrand.TabIndex = 5;
+            // 
+            // btnMARS
+            // 
+            this.btnMARS.Location = new System.Drawing.Point(246, 37);
+            this.btnMARS.Name = "btnMARS";
+            this.btnMARS.Size = new System.Drawing.Size(75, 23);
+            this.btnMARS.TabIndex = 6;
+            this.btnMARS.Text = "MARS";
+            this.btnMARS.UseVisualStyleBackColor = true;
+            this.btnMARS.Click += new System.EventHandler(this.btnMARS_Click);
+            // 
+            // btnReadfromXML
+            // 
+            this.btnReadfromXML.Location = new System.Drawing.Point(246, 67);
+            this.btnReadfromXML.Name = "btnReadfromXML";
+            this.btnReadfromXML.Size = new System.Drawing.Size(103, 23);
+            this.btnReadfromXML.TabIndex = 7;
+            this.btnReadfromXML.Text = "XML\'den Oku";
+            this.btnReadfromXML.UseVisualStyleBackColor = true;
+            // 
+            // btnSavetoXML
+            // 
+            this.btnSavetoXML.Location = new System.Drawing.Point(316, 311);
+            this.btnSavetoXML.Name = "btnSavetoXML";
+            this.btnSavetoXML.Size = new System.Drawing.Size(103, 23);
+            this.btnSavetoXML.TabIndex = 8;
+            this.btnSavetoXML.Text = "XML\'e Kaydet";
+            this.btnSavetoXML.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Color";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Brand";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 365);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSavetoXML);
+            this.Controls.Add(this.btnReadfromXML);
+            this.Controls.Add(this.btnMARS);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.Button btnMARS;
+        private System.Windows.Forms.Button btnReadfromXML;
+        private System.Windows.Forms.Button btnSavetoXML;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Ders29/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMARS_Click(object sender, EventArgs e)
+        {
+            string strConn = "data source=.;initial catalog=AdventureWorks2016CTP3;integrated security=true;MultipleActiveResultSets=true;";
+
+            //aynı anda birden fazla bağlantı kullanılacaksa multiple...
+
+            SqlConnection conn = new SqlConnection(strConn);
+
+            conn.Open();
+
+            SqlCommand cmd1 = new SqlCommand("select ProductCategoryID,Name from Production.ProductCategory", conn);
+
+            SqlDataReader dr1 = cmd1.ExecuteReader();
+
+            while(dr1.Read())
+            {
+               //treeView1.Nodes.Add(dr1[1].ToString());
+
+                TreeNode node = new TreeNode(dr1[1].ToString());
+
+               SqlCommand cmd2 = new SqlCommand("select ProductSubCategoryID,ProductCategoryID,Name from Production.ProductSubCategory  where ProductCategoryID=@ID", conn);
+
+               cmd2.Parameters.Clear();
+
+              cmd2.Parameters.AddWithValue("@ID", dr1[0]);
+
+              SqlDataReader dr2 = cmd2.ExecuteReader();
+
+               while(dr2.Read())
+               {
+                    node.Nodes.Add(dr2[2].ToString());
+              }
+               if(treeView1.Nodes.Count <4)
+                {
+                    treeView1.Nodes.Add(node);
+                }
+
+                
+            }
+
+            conn.Close();
+        }
+    }
+}
