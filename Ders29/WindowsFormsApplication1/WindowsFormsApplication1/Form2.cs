e8a65e1c967105f6d6c3eec294b4d43b00f049d6
srkans <serkansacma@hotmail.com>
2022-12-07 14:43:54 +0300

    dataset-xmlschema-xmlreadwrite

---
+++ Ders29/WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strConn = "data source=SERKAN;initial catalog=AdventureWorks2016CTP3;integrated security=true;";
+
+            SqlDataAdapter da = new SqlDataAdapter("select ProductID,Name,Color,ListPrice from Production.Product", strConn);
+
+            //DataTable dt = new DataTable("Product");
+            DataSet dt = new DataSet();
+
+            da.Fill(dt);
+
+            //dataGridView1.DataSource = dt;
+
+            dt.WriteXml("product.xml");
+            dt.WriteXmlSchema("product.xsd");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //DataTable dt = new DataTable();
+            DataSet dt = new DataSet();
+
+            //dt.ReadXmlSchema("product.xsd");
+            dt.ReadXml("product.xml");
+
+            dataGridView1.DataSource = dt.Tables[0];
+        }
+    }
+}
