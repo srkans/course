0a5775b9cb97dbccab828b6155bb5d7010c6ccef
srkans <serkansacma@hotmail.com>
2022-12-07 09:05:47 +0300

    datatable-datarow- sqldataadapter - sqlcommandbuilder

---
+++ Ders28/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            string strConn = "Data Source = SERKAN; Initial Catalog = Calisma; Integrated Security = True";
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Kitaplar", strConn);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            DataRow row = dt.NewRow();
+
+            row[0] = 963;
+            row[1] = 369;
+            row[2] = "House of Dragon";
+
+            dt.Rows.Add(row);
+
+            dataGridView1.DataSource = dt;
+
+            SqlCommandBuilder scb = new SqlCommandBuilder(da);
+
+            StreamWriter sw = new StreamWriter("data.txt");
+
+            foreach(DataRow r in dt.Rows)
+            {
+                sw.WriteLine(r[2].ToString() + " " + row.RowState);
+            }
+
+            sw.Close();
+
+            da.Update(dt);
+        }
+    }
+}
---
+++ Ders28/WindowsFormsApplication1/WindowsFormsApplication1/Kitap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Kitap
+    {
+        public int KitapID { get; set; }
+        public string KitapAdi { get; set; }
+        public double? Fiyat { get; set; }  //? allow null
+
+    }
+}
