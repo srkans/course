b622767f41169db65f2cbd91382af3fd005e13c6
srkans <serkansacma@hotmail.com>
2022-12-07 15:10:18 +0300

    dataset-datagridviewdatasource-bindingNavigator

---
+++ Ders29/WindowsFormsApplication1/WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'calismaDataSet.Kitaplar' table. You can move, or remove it, as needed.
+            this.kitaplarTableAdapter.Fill(this.calismaDataSet.Kitaplar);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kitaplarTableAdapter.Update(calismaDataSet);
+        }
+    }
+}
