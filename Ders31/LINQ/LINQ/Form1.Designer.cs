98be91aa1b02268f57df1e721b818198b0b03916
srkans <serkansacma@hotmail.com>
2022-12-09 11:45:20 +0300

    var - dynamic

---
+++ Ders31/LINQ/LINQ/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnVar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(326, 186);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 15;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(326, 129);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(116, 41);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Lambda Expression";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(326, 54);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 43);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Extension Methods";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(83, 271);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 47);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Anonymous Types";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 40);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Collection Initializer";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(99, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 36);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Object Initializer";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Auto Property";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnVar
+            // 
+            this.btnVar.Location = new System.Drawing.Point(115, 40);
+            this.btnVar.Name = "btnVar";
+            this.btnVar.Size = new System.Drawing.Size(51, 43);
+            this.btnVar.TabIndex = 8;
+            this.btnVar.Text = "var";
+            this.btnVar.UseVisualStyleBackColor = true;
+            this.btnVar.Click += new System.EventHandler(this.btnVar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 358);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnVar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnVar;
+    }
+}
+
---
+++ Ders31/LINQ/LINQ/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVar_Click(object sender, EventArgs e)
+        {
+            var sayi = 10;
+            var mesaj = "Deneme 123";
+            var maas = 1900.99;
+            var kontrol = true;
+            var harf = 'a';
+
+            //tip runtime a cıkıncaya kadar belli değil uygulama calıstıgı anda tip belli oluyor ve baska bir tip almıyor. var olarak tanımlayınca ..
+
+            dynamic deneme = 99;
+            deneme = "Merhaba Dünya "; //dynamic runtime da farklı tipler alabiliyor.
+
+        }
+    }
+}
