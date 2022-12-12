d0789354f49b3ccfdb2fca73c7599426064c7da9
srkans <serkansacma@hotmail.com>
2022-12-12 14:15:21 +0300

    dynamicTable asp.net

---
+++ Ders34/dinamikTablo.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="dinamikTablo.aspx.cs" Inherits="dinamikTablo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Satır :
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Sütun :
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Olustur" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Ders34/dinamikTablo.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class dinamikTablo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write("<table border = 1>");
+
+        for (int i = 0; i < int.Parse(TextBox1.Text); i++)
+        {
+            if(i%2 == 1)
+            {
+                Response.Write("<tr bgcolor = #aaaaaa>");
+            }
+            else
+            {
+                Response.Write("<tr bgcolor = orange>");
+            }
+
+            for(int j = 0; j< int.Parse(TextBox2.Text); j++)
+            {
+                Response.Write("<td>Betül</td>");
+            }
+            Response.Write("</tr>");
+        }
+        Response.Write("</table>");
+
+    }
+}
