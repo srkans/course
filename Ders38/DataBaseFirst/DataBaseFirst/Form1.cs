44ffadd0104378c4830e315432b0cc7eae292381
srkans <serkansacma@hotmail.com>
2022-12-15 12:25:16 +0300

    database first entity framework

---
+++ Ders38/DataBaseFirst/DataBaseFirst/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBaseFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calismaEntities db = new calismaEntities();
+
+            var sonuc = from p in db.Personel where p.PerAd.Contains(txtName.Text) select p;
+
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+    }
+}
