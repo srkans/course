2ec2015156979dbaa688713fa54937cd72bf0b07
srkans <serkansacma@hotmail.com>
2022-12-15 16:38:18 +0300

    asp.net mvc- wcf Service practice

---
+++ Ders38/WCF/KurlarMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurlarMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (KurServis.KurServisClient servisClient = new KurServis.KurServisClient())
+            {
+                if(TempData["Kurlar"]!=null)
+                {
+                    ViewBag.Kurlar = (List<double>)TempData["Kurlar"];
+                }
+                else
+                {
+                    ViewBag.Kurlar = new List<double>();
+                }
+                var model = servisClient.paraBirimleriGetir().ToList();
+                return View(model);
+            }
+            
+        }
+
+        public ActionResult KurGetir(string paraBirimi)
+        {
+            using (KurServis.KurServisClient servisClient = new KurServis.KurServisClient())
+            {
+                var model = servisClient.KurlariGetir(paraBirimi).ToList();
+
+                TempData["Kurlar"] = model;
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
