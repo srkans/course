e60d8af6e4f57777625d5282ae347600f3dbcba6
srkans <serkansacma@hotmail.com>
2022-12-20 16:02:34 +0300

    JSON - async - await

---
+++ Ders43/JSON/jsonExample/Post.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonExample
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+   
+    public class Post
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+}
---
+++ Ders43/JSON/jsonExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace jsonExample
+{  
+    class Program
+    {
+
+
+        public static async Task Main(string[] args) //async
+        {
+            string url = "https://my-json-server.typicode.com/typicode/demo/posts";
+
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                var httpResponseMessage = await httpClient.GetAsync(url);
+
+                string jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                Console.WriteLine(jsonResponse);
+
+                var myPosts = JsonConvert.DeserializeObject<Post[]>(jsonResponse);
+
+                foreach(var post in myPosts)
+                {
+                    Console.WriteLine($"{post.Id} {post.Title}");
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+
+        }
+        
+    }
+}
